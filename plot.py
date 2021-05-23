a50d6d75e344060d40717fa7d5a4d8dda1720659
michbelle <mi94bell@gmail.com>
2021-05-23 19:43:05 +0200

    test1

---
+++ plot.py
@@ -0,0 +1,15 @@
+import numpy  as np
+import matplotlib.pyplot as plt
+data = np.loadtxt('data.txt')
+
+
+x = data[:, 0]
+y1 = data[:, 1]
+y2 = data[:, 2]
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(x, y1,'r--')
+plt.subplot(212)
+plt.plot(x, y2,'g--')
+plt.show()
